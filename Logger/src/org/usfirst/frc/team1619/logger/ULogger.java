0f71044e784a4175b9297efe9e6f11a9e52c171b
Daniel Hathcock <danielperson7@gmail.com>
2015-10-06 21:20:23 -0600

    Added Logger intellij project. Added strict .gitignore (may need to be edited) to repo, and a more specific gitignore for Logger

---
+++ Logger/src/org/usfirst/frc/team1619/logger/ULogger.java
@@ -0,0 +1,11 @@
+package org.usfirst.frc.team1619.logger;
+
+/**
+ * Created by DanielHathcock on 10/6/15.
+ * Project: Logger
+ * Package: org.usfirst.frc.team1619.logger
+ */
+public class ULogger
+{
+    
+}
